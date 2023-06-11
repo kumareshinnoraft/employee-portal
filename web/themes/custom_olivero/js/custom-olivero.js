b9d1bf751ed10a7222a79dbd8e5b255c724903e9
kumareshinnoraft <kumareh.baksi@innoraft.com>
2023-06-11 08:47:06 +0530

    FT2023-269: Employee project completed and split configuration exported.

---
+++ web/themes/custom_olivero/js/custom-olivero.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * custom_olivero behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.customOlivero = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
